5bd0704844ac255cea12593b1c84ab3d8a7de8cf
badido18 <hm_boudis@esi.dz>
2022-08-13 17:18:17 +0200

    Assets + Front + Colors

---
+++ getheplate-app/getheplate-app/src/colors.js
@@ -0,0 +1,15 @@
+const Colors = {
+    white : "#FFFFFF",
+    black : "#000000",
+    grey : "#444444",
+    dark_grey : "#363636",
+    light_grey : "#F3F3F3",
+    green : "#00AD07",
+    red : "#B20000",
+    blue : "#003498",
+    yellow : "#C2C600"
+}
+
+
+
+export default Colors
