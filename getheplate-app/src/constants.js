9c7d37e2f40b0fcefced8eb3c2d4260c686f4885
Madjid Boudis <34186054+badido18@users.noreply.github.com>
2022-10-05 11:42:43 +0200

    Update constants.js

--- getheplate-app/src/constants.js
+++ getheplate-app/src/constants.js
@@ -1,7 +1,7 @@
 export const LOCAL_URL = "http://127.0.0.1"
-export const BASE_URL = "http://35.232.72.59"
+export const BASE_URL = "http://xxx.xxx.xxx.xxx"
 export const MODEL_PORT = 5000 
 export const BE_PORT = 8081
 export const JWT_SECRET = 'gtp-back'
-export const IMGBB_API_KEY = 'af3cccbb5d69f50604e0ae0195dbb4cd'
-export const MAPS_API_KEY = 'AIzaSyC6-04YyaLTY1Hg1AgYbjpOMD6oeeB5U7M'
+export const IMGBB_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+export const MAPS_API_KEY = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyy'
